441bc841ec197f1d5f2fd993b9aa474c7d94cf53
Paulo Angelo <pa@pauloangelo.com>
2018-06-02 18:48:09 -0300

    save

--- auth.php
+++ auth.php
@@ -85,7 +85,7 @@ class auth_plugin_guardiankey extends auth_plugin_base {
           $json->psychometricTyped="";
           $json->psychometricImage="";
      
-          $message = json_encode($json);
+          $message = json_encode($json,JSON_UNESCAPED_SLASHES);
           $cipher = openssl_encrypt($message, AES_256_CBC, $key, 0, $iv);
           $payload=$hashid."|".$cipher;
           $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
